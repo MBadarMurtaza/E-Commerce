6aeca973e15ca565ff7bf41f2c18cbe91a9b0bc5
mBadar-dev <m.badar.dev@gmail.com>
2025-09-03 12:33:15 +0500

    adding context Provider

--- front_end/src/main.jsx
+++ front_end/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ShopContext } from "./Context/ShopContext.jsx";
+import ShopContextProvider from "./Context/ShopContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </StrictMode>
+);
