5015605f154be504c3f1e19dbd6153426931f03c
mBadar-dev <m.badar.dev@gmail.com>
2025-09-03 13:16:08 +0500

    adding more protected routing

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -9,37 +9,72 @@ import Footer from "./Components/Footer/Footer";
 import men_banner from "./Components/Assets/banner_mens.png";
 import women_banner from "./Components/Assets/banner_women.png";
 import kid_banner from "./Components/Assets/banner_kids.png";
+import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
 
 function App() {
+  const isLoggedIn = !!localStorage.getItem("auth-token"); // simple check
+
   return (
-    <>
-      <div>
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Shop />} />
-            <Route
-              path="/mens"
-              element={<ShopCategory banner={men_banner} category="men" />}
-            />
-            <Route
-              path="/womens"
-              element={<ShopCategory banner={women_banner} category="women" />}
-            />
-            <Route
-              path="/kids"
-              element={<ShopCategory banner={kid_banner} category="kid" />}
-            />
-            <Route path="product" element={<Product />}>
-              <Route path=":productid" element={<Product />} />
-            </Route>
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/login" element={<LoginSignup />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </div>
-    </>
+    <BrowserRouter>
+      {/* Only show Navbar if logged in */}
+      {isLoggedIn && <Navbar />}
+
+      <Routes>
+        <Route path="/login" element={<LoginSignup />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Shop />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/mens"
+          element={
+            <ProtectedRoute>
+              <ShopCategory banner={men_banner} category="men" />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/womens"
+          element={
+            <ProtectedRoute>
+              <ShopCategory banner={women_banner} category="women" />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/kids"
+          element={
+            <ProtectedRoute>
+              <ShopCategory banner={kid_banner} category="kid" />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoute>
+              <Cart />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="product/:productid"
+          element={
+            <ProtectedRoute>
+              <Product />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+
+      {/* Only show Footer if logged in */}
+      {isLoggedIn && <Footer />}
+    </BrowserRouter>
   );
 }
 
